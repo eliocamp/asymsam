80784031a205ba4b230d9c1db76715e72159a659
eliocamp <eliocampitelli@gmail.com>
2020-06-25 19:03:53 -0300

    Adds some figures

--- R/theme.R
+++ R/theme.R
@@ -9,17 +9,17 @@
 theme_asymsam <- function(base_size = 16) {
   ggplot2::theme_minimal(base_size = 16) +
     ggplot2::theme(
-      strip.background = ggplot2::element_rect(fill = NA, color = "gray30"),
+      # strip.background = ggplot2::element_rect(fill = NA, color = "gray30"),
       # text = element_text(family = font_rc),
       legend.position = "bottom", legend.box = "vertical",
-      panel.spacing.y = grid::unit(5, "mm"),
-      panel.spacing.x = grid::unit(5, "mm"),
-      legend.spacing = grid::unit(2, "mm"),
-      panel.border = ggplot2::element_rect(colour = "black", fill = NA),
-      plot.margin = grid::unit(rep(3, 4), "mm"),
-      # legend.title = element_blank(),
-      legend.box.spacing = grid::unit(3, "mm"),
-      legend.margin = ggplot2::margin(t = -5),
+      # panel.spacing.y = grid::unit(5, "mm"),
+      # panel.spacing.x = grid::unit(5, "mm"),
+      # legend.spacing = grid::unit(2, "mm"),
+      # panel.border = ggplot2::element_rect(colour = "black", fill = NA),
+      # plot.margin = grid::unit(rep(3, 4), "mm"),
+      # # legend.title = element_blank(),
+      # legend.box.spacing = grid::unit(3, "mm"),
+      # legend.margin = ggplot2::margin(t = -5),
       panel.grid = ggplot2::element_line(color = "gray10", size = 0.4, linetype = 3),
       panel.ontop = TRUE)
 }
