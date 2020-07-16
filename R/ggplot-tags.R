d37ca0e98ef5b8823989f16d2c6a0243fadf6298
eliocamp <eliocampitelli@gmail.com>
2020-07-16 18:14:18 -0300

    more results

--- R/ggplot-tags.R
+++ R/ggplot-tags.R
@@ -1,7 +1,8 @@
 #' Adds tags to facets
 #'
 #' @export
-facet_tags <- function(position = "tl", tag_pool = letters, open = "", close = ")", stroke = 0.15, stroke.color = "white",
+tag_facets <- function(position = "tl", tag_levels = "a", tag_pool = NULL, tag_prefix = "",
+                       tag_suffix = ")", stroke = 0.15, stroke.color = "white",
                  color = "black") {
   if (!is.list(position)) {
     position <- switch(position,
@@ -13,7 +14,8 @@ facet_tags <- function(position = "tl", tag_pool = letters, open = "", close = "
   }
 
 
-  structure(list(tag_pool = tag_pool, open = open, close = close, position = position,
+  structure(list(tag_pool = tag_pool, tag_levels = tag_levels, open = tag_prefix,
+                 close = tag_suffix, position = position,
                  color = color,
                  stroke = stroke, stroke.color = stroke.color),
             class = "ggtags")
@@ -40,25 +42,43 @@ print.ggtagged <- function(x, newpage = is.null(vp), vp = NULL, ...) {
   )
 
   tag_options <- attr(x, "tag_options")
+
   gb <- ggplot2::ggplot_build(x)
   lay <- gb$layout$layout
 
-  x = tag_options$position$x
-  y = tag_options$position$y
+  x <-  tag_options$position$x
+  y <-  tag_options$position$y
 
-  hjust = tag_options$position$hjust
-  vjust = tag_options$position$vjust
+  hjust <- tag_options$position$hjust
+  vjust <- tag_options$position$vjust
 
   gt <- ggplot2::ggplot_gtable(gb)
   panels <- which(grepl("panel", gt$layout$name))
+
+  i_panels <- seq_along(panels)
+
+  if (is.null(tag_options$tag_pool)) {
+   tag_pool <-  switch(tag_options$tag_levels,
+                       a = letters[i_panels],
+                       A = LETTERS[i_panels],
+                       "1" = i_panels,
+                       "i" = tolower(utils::as.roman(i_panels)),
+                       "I" = utils::as.roman(i_panels),
+                       stop("tag_levels is not valis")
+                       )
+
+  } else {
+    tag_pool <- tag_options$tag_pool
+  }
+
   for (p in seq_along(panels)) {
 
     if (tag_options$stroke == 0) {
-      tagGrob <- grid::textGrob(paste0(tag_options$open, tag_options$tag_pool[p], tag_options$close),
+      tagGrob <- grid::textGrob(paste0(tag_options$open, tag_pool[p], tag_options$close),
                                             x = x, y = y, gp = grid::gpar(col = tag_options$color),
                                             hjust = hjust, vjust = vjust)
     } else {
-      tagGrob <- shadowtext::shadowtextGrob(paste0(tag_options$open, tag_options$tag_pool[p], tag_options$close),
+      tagGrob <- shadowtext::shadowtextGrob(paste0(tag_options$open, tag_pool[p], tag_options$close),
                                             x = x, y = y, bg.r = tag_options$stroke, bg.colour = tag_options$stroke.color,
                                             gp = grid::gpar(col = tag_options$color),
                                             hjust = hjust, vjust = vjust)
