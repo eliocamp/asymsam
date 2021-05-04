34381963e6c6f6923bbd4050e6400d880ba322fc
Elio Campitelli <eliocampitelli@gmail.com>
2021-05-04 14:57:11 -0300

    Agrega temperatura de ERA5

--- analysis/scripts/01-get-data.R
+++ analysis/scripts/01-get-data.R
@@ -16,7 +16,8 @@ files <- list(aao = "aao.csv",
               hadcrut = "HadCRUT.4.6.0.0.median.nc",
               gpcc = "precip.mon.total.v2018.nc",
               gpcp = "gpcp.precip.mon.mean.nc",
-              era5 =  "era5.z.mon.mean.nc")
+              era5 =  "era5.z.mon.mean.nc",
+              era52mt = "era5.2mt.mon.mean.nc")
 
 dir.create(asymsam::data_path("raw"), recursive = TRUE, showWarnings = FALSE)
 dir.create(asymsam::data_path("derived"), recursive = TRUE, showWarnings = FALSE)
@@ -144,85 +145,43 @@ if (!force && file.exists(asymsam::data_path("raw", file))) {
                               path = asymsam::data_path())
 }
 
-#
-# for (y in seq(1979, 2018)) {
-#   url <- paste0("ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.dailyavgs/pressure/hgt.", y, ".nc")
-#   dir.create(asymsam::data_path("raw", "ncep"), )
-#   file <- asymsam::data_path("raw", "ncep", paste0("hgt.daily.", y, ".nc"))
-#   if (!file.exists(file)) {
-#     download.file(url, destfile = file)
-#   }
-# }
-#
-# ncepfiles <- list.files(asymsam::data_path("raw", "ncep"), full.names = TRUE)
-#
-# asymsam::nc_concatenate(ncepfiles, asymsam::data_path("raw", "ncep.daily.nc"), overwrite = TRUE)
 
-# # Antarctic sea ice
-# # from https://nsidc.org/data/G02135
-# months <- 1:12
-# years <- 1979:2019
-# dates <- data.table::CJ(years, months)
-# # There are no data from 3 December 1987 to 13 January 1988 due to satellite problems. :sob:
-# dates <- dates[!(paste0(years, months) %in% c(198712, 19881))]
-#
-# make_url <- function(year, month) {
-#   month_zero <- formatC(month, width = 2, flag = "0")
-#   month_Abb <- month.abb[month]
-#
-#   file_name <- paste0("S_", paste0(year, month_zero), "_concentration_v3.0.tif")
-#   url <- paste0("ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/south/monthly/geotiff/",
-#                 month_zero, "_", month_Abb, "/", file_name)
-#
-#   list(file_name,
-#        url)
-# }
-#
-# dates[, c("file_name", "url") := make_url(years, months)]
-# p <- txtProgressBar(max = 100, style = 3)
-#
-# end_file <- asymsam::data_path("derived", "ice.Rds")
-# if (!file.exists(end_file)) {
-#   ices <- lapply(seq_len(nrow(dates)), function(i) {
-#     file <- asymsam::data_path("raw", "ice", dates$file_name[i])
-#     dir.create(asymsam::data_path("raw", "ice"), showWarnings = FALSE)
-#     if (!file.exists(file)) {
-#       download.file(dates$url[i], file)
-#     }
-#     # file
-#     ice <- raster::raster(file)
-#
-#     proj <<- as.character(raster::crs(ice))
-#
-#     r <- data.table::as.data.table(raster::as.data.frame(ice, xy = TRUE, long = TRUE))
-#     r[, layer := NULL]
-#     r[, time := lubridate::make_datetime(dates$years[i], dates$months[i])]
-#
-#
-#     no_ice <- c(pole = 2510,
-#                 coast = 2530,
-#                 land  = 2540,
-#                 ocean = 0)
-#     # Values from 1-150 (1% to 15% concentration) are statistically irrelevant
-#     # because data values less than 15% from passive microwave instruments are too
-#     # uncertain to use, so these should be ignored
-#     missing <-  c(2550) #
-#
-#     r <- r[!(value %in% no_ice)]
-#     r[value %in% missing, value := NA]
-#     r[, value := value/10/100]  # divide by 10 to get percent
-#     setTxtProgressBar(p, i/nrow(dates)*100)
-#     r[]
-#
-#   })
-#   close(p)
-#   ice <- data.table::rbindlist(ices)
-#
-#
-#   ice[, c("lon", "lat") := proj4::project(list(x, y), proj = proj, inverse = TRUE)]
-#   ice[, lon := metR::ConvertLongitude(lon)]
-#   data.table::setnames(ice, "value", "concentration")
-#
-#   saveRDS(ice, end_file)
-#   saveRDS(proj, asymsam::data_path("derived", "ice_proj.Rds"))
-# }
+# Download ERA5 temperature -----------------------------------------------
+file <- files$era52mt
+
+if (!force && file.exists(asymsam::data_path("raw", file))) {
+  message("File already exists.")
+} else {
+  if (is.na(user) | is.na(key)) {
+    stop("Please set the environmental variables CDSUSER and CDSKEY")
+  }
+
+  ecmwfr::wf_set_key(user = user,
+                     key = key,
+                     service = "cds")
+  request <- list(
+    format = "netcdf",
+    product_type = "monthly_averaged_reanalysis",
+    variable = c("2m_temperature"),
+    year = c("1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986",
+             "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994",
+             "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002",
+             "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010",
+             "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"),
+    month = c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"),
+    time = "00:00",
+    grid = c("2.5", "2.5"),
+    dataset_short_name = "reanalysis-era5-single-levels-monthly-means",
+    target = file
+  )
+
+
+  files <- ecmwfr::wf_request(request = request,
+                              user = user,
+                              time_out = 5*3600,
+                              path = asymsam::data_path())
+}
+
+
+
+
