8811e1669b3a74a589ee6c90309d6df4ad7130c9
Elio Campitelli <eliocampitelli@gmail.com>
2021-05-04 20:22:11 -0300

    Knitea

--- analysis/AGU/agu-abstract.R
+++
@@ -1,8 +0,0 @@
----
-title: "Teasing out the asymmetric component of the Southern Annular Mode"
-output: pdf_document
----
-
-The conception of the Southern Annular Mode as a mostly zonally symmetric pattern is reflected not only in its name, but also on the various methods used to characterise it. Gong and Wang (1999) and many others define the SAM index based on zonal mean pressure anomalies. Baldwind & Thompson also put forward an index based on performin EOF on zonal mean geopotential height. However, composites, as well as regression patterns, based on these indinces un
-[@fogt2012 ]
-. Here, we propose a straightforward method of 
